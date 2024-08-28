92ffeb451d79f8cdcf5480328b39c70ebc692441
Cyril Dumange <cyril.dumange@zenika.com>
2024-08-28 10:42:31 -0400

    chore: configuration for cluster mode

--- Consumer/Program.cs
+++ Consumer/Program.cs
@@ -1,30 +1,12 @@
-using System.Net.Http.Headers;
-using System.Text.Json;
-using System.Text.Json.Serialization;
+
 using Confluent.Kafka;
-using Consumer.Components;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
 
 using var client = new ConsumerBuilder<string, string>(new ConsumerConfig
 {
     AllowAutoCreateTopics = true,
-    BootstrapServers = "127.0.0.1:9092",
-    GroupId = "hey-aa",
+    BootstrapServers = "kafka-1:9092",
+    GroupId = "hey-aaa",
     AutoOffsetReset = AutoOffsetReset.Earliest,
 
 }).Build();
